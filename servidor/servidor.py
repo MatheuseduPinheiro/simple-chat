84e18f9a2672c94728e8481ec22e2beca5d222b4
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2024-09-10 12:56:59 -0300

    Personalização das mensagens tanto no servidor quanto no cliente e escalabilidade para múltiplas mensagens com suporte para até 5 conexões

--- servidor/servidor.py
+++ servidor/servidor.py
@@ -1,24 +1,31 @@
 import socket
 
-#Criando um socket TCP/IP
+# Criando um socket TCP/IP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Vinculando o socket ao endereço e porta 
-server_socket.bind(("127.0.0.1",12345))
+# Vinculando o socket ao endereço e porta 
+server_socket.bind(("127.0.0.1", 12345))
 
-#Executando por conexões
+# Executando por conexões
 server_socket.listen(5)
-print(f"Servidor escutando na porta...")
+print("Servidor escutando na porta 12345...")
 
-#Aceitando conexões de clientes
 while True:
-    conn , addr = server_socket.accept()
-
+    conn, addr = server_socket.accept()
     print(f"Conectado por {addr}")
 
-    #Recebendo mensagem do cliente
-    data = conn.recv(1024)
-    print(f"Mensagem recebida: {data.decode()}")
+    try:
+        while True:
+            # Recebendo mensagem do cliente
+            data = conn.recv(1024)
+            if not data:
+                print(f"Desconexão detectada para {addr}")
+                break
+            print(f"Mensagem recebida do {addr}: {data.decode()}")
 
-    #Respondendo ao  cliente
-    conn.sendall(b"Mensagem recebida pelo servidor")
-    conn.close()
+            # Respondendo ao cliente
+            conn.sendall(b"Mensagem recebida pelo servidor")
+    except Exception as e:
+        print(f"Erro durante a comunicação: {e}")
+    finally:
+        # Fechando a conexão com o cliente
+        conn.close()
